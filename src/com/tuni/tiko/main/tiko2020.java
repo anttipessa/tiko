86e1b51c1a9fd20c653e65399d2c03062bc815d3
Antti <anttipessa@gmail.com>
2020-03-27 13:09:33 +0200

    Add sql template

--- src/com/tuni/tiko/main/tiko2020.java
+++ src/com/tuni/tiko/main/tiko2020.java
@@ -3,6 +3,6 @@ package com.tuni.tiko.main;
 public class tiko2020 {
     public static void main(String[] args) {
         System.out.println("Welcome to the database!");
-
+        
     }
 }
--- src/com/tuni/tiko/test/test.java
+++ src/com/tuni/tiko/test/test.java
@@ -1,5 +1,7 @@
 package com.tuni.tiko.test;
 
 public class test {
+    public static void main(String[] args) {
 
+    }
 }
