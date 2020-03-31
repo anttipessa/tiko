6740b8d64d9841819c645d2c28cf2a85531001fb
Risto Meltaus <risto.meltaus@tuni.fi>
2020-03-31 19:53:00 +0300

    gui aluille
    Graafisen käyttöliittymän alku: näkymiä, nappuloita, tekstin lukemista.

--- src/main/java/Tiko2020.java
+++ src/main/java/Tiko2020.java
@@ -1,5 +1,8 @@
 public class Tiko2020 {
     public static void main(String[] args) {
         System.out.println("Tietokantaohjelmointi 2020");
+        
+        GUI gui = new GUI();
+        gui.setVisible(true);
     }
 }
